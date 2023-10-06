ba3eb62d54e07402aed06f945b54482e96dcc5ee
Maximilian Kraft <kraft@spengergasse.at>
2023-10-06 08:04:49 +0200

    initial commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,19 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Telekomlab6 on 04.10.23.
+//
+
+import SwiftUI
+
+
+// einstiegspunkt der app, erkennbar an der @main annotation
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
